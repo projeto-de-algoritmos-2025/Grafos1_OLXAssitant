f2f25e51328dd9dbb68af244f091c9e33eddc6ad
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2025-04-18 23:32:49 -0300

    Implementação básica de grafos

---
+++ grafo.py
@@ -0,0 +1,36 @@
+import json
+from collections import defaultdict
+
+class Grafo:
+    """Implementação simples de grafos com representação em dicionários e algoritmos de comunidade para classificação de nós e cálculo de similaridades"""
+    
+    def __init__(self):
+        # Dicionário para armazenar nós {node_id: {attributes}}
+        self.nodes = {}
+        # Dicionário para armazenar arestas {node_id: {neighbor_id: {attributes}}}
+        self.edges = defaultdict(dict)
+    
+    def __iter__(self):
+        return iter(self.nodes)
+    
+    def add_node(self, node_id, **attributes):
+        # Adiciona um nó com atributos ao grafo
+        self.nodes[node_id] = attributes
+    
+    def add_edge(self, node1, node2, **attributes):
+        # Adiciona uma aresta entre node1 e node2 com atributos
+        self.edges[node1][node2] = attributes
+        self.edges[node2][node1] = attributes  # Grafo não direcionado
+    
+    def neighbors(self, node_id):
+        # Obtém vizinhos de um nó
+        return self.edges[node_id].keys()
+    
+    def number_of_nodes(self):
+        # Obtém o número de nós no grafo
+        return len(self.nodes)
+    
+    def number_of_edges(self):
+        # Obtém o número de arestas no grafo
+        return sum(len(neighbors) for neighbors in self.edges.values()) // 2  # Divide por 2 para grafo não direcionado
+    
